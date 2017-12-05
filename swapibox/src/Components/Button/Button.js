1b49fad311b8f674a5d4f1d09fab8bd9c1c28636
Jason Hughes <jasonhughes088@gmail.com>
2017-12-04 21:20:18 -0700

    film text rendering in scroll component

---
+++ swapibox/src/Components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import css from './Button.css';
+
+const Button = () => {
+  return (
+    <div>
+      <button>People</button>
+      <button>Planets</button>
+      <button>Vehicles</button>
+    </div>
+  )
+}
+
+
+export default Button;
