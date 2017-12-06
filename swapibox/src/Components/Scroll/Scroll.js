40a8c1622fae7874cfe0d3c8d6113832b75c691d
Jason Hughes <jasonhughes088@gmail.com>
2017-12-05 17:05:45 -0700

    refactor to bring in a cleaner function and now the film being displayed is random

--- swapibox/src/Components/Scroll/Scroll.js
+++ swapibox/src/Components/Scroll/Scroll.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import css from './Scroll.css';
 
-const Scroll = ({scrollData}) => {
-  console.log(scrollData);
+const Scroll = ({data, opening}) => {
+const text = data[opening].Opening;
+const filmTitle = data[opening].Title;
+const releaseDate = data[opening].Release;
+
   return (
     <div className="scroll-container">
-      <p className="opening-crawl">{scrollData}</p>
+      <p className="opening-crawl">{text}</p>
     </div>
 
   )
