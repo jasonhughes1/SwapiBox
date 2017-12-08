60e6606ff556b0316fbc77b490f9cadb5b6a2064
Jason Hughes <jasonhughes088@gmail.com>
2017-12-08 13:04:19 -0700

    button css fix and center background image

--- swapibox/src/Components/Header/Header.js
+++ swapibox/src/Components/Header/Header.js
@@ -6,15 +6,15 @@ import Controls from './../Controls/Controls.js'
 const Header = ({favFn, numFav, changeCards, num}) => {
   return (
     <div className="header-container">
+      <h1 className="main-header">SWAPI-Box</h1>
       <div className="header">
-        <h1 className="main-header">SWAPI-Box</h1>
-        <Controls className={' button favorite '}
-          buttonText={'View Favorites' + numFav}
+      </div>
+        <Controls className={' button-favorite '}
+          buttonText={'View Favorites:  ' + numFav}
           favFn={favFn}
           changeCards={changeCards}
           num={num}
         />
-      </div>
     </div>
   );
 };
