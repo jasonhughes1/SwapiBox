739313627a359050f261bd0668c105396cba2356
Jason Hughes <jasonhughes088@gmail.com>
2017-12-07 23:21:18 -0700

    google font added to application

--- swapibox/src/Components/Header/Header.js
+++ swapibox/src/Components/Header/Header.js
@@ -6,13 +6,15 @@ import Controls from './../Controls/Controls.js'
 const Header = ({favFn, numFav, changeCards, num}) => {
   return (
     <div className="header-container">
-      <h1 className="main-header">SWAPI-Box</h1>
-      <Controls className={' button favorite '}
-        buttonText={'View Favorites' + numFav}
-        favFn={favFn}
-        changeCards={changeCards}
-        num={num}
-      />
+      <div className="header">
+        <h1 className="main-header">SWAPI-Box</h1>
+        <Controls className={' button favorite '}
+          buttonText={'View Favorites' + numFav}
+          favFn={favFn}
+          changeCards={changeCards}
+          num={num}
+        />
+      </div>
     </div>
   );
 };
