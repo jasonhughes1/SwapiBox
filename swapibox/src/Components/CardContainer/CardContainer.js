98fec425c66dc10526c25c1d4f6633bbe5dccd90
Jason Hughes <jasonhughes088@gmail.com>
2017-12-11 12:55:48 -0700

    toggle class on favs working

--- swapibox/src/Components/CardContainer/CardContainer.js
+++ swapibox/src/Components/CardContainer/CardContainer.js
@@ -5,16 +5,20 @@ import PropTypes from 'prop-types';
 
 
 const CardContainer = ({ cardType, setFavorite, favArray}) => {
-  // const favClass = favArray.filter(card => {
-  //   card ===
+
 
   if (cardType.length > 0) {
-    const cards = cardType.map((card, index) =>
-      <Card cardData={card}
+    const cards = cardType.map((card, index) => {
+      let favClass = (favArray.find(favCard => {
+        return card === favCard;
+      })) ? 'active' : 'inactive';
+      return <Card
+        favClass={favClass}
+        cardData={card}
         key={index}
         setFavorite={setFavorite}
-      />
-    );
+      />;
+    });
 
     return (
       <div className='card-container'>
@@ -30,7 +34,8 @@ const CardContainer = ({ cardType, setFavorite, favArray}) => {
 
 CardContainer.propTypes = {
   cardType: PropTypes.array,
-  setFavorite: PropTypes.func
+  setFavorite: PropTypes.func,
+  favArray: PropTypes.array
 };
 
 export default CardContainer;
